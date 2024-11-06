89ea0db27910e71f750cdc78304159e84d3fd71d
Pop Lucas <lucaspop5511@gmail.com>
2024-11-07 00:42:19 +0200

    another test commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,6 @@ import React from 'react';
 function About() {
     return (
         <section style={{ marginBottom: '2rem' }}>
-            <h2>About Me</h2>
             <p>
                 Hi! I'm Lucas, a web designer and student passionate about building interactive and
                 visually engaging web experiences. I’m currently based in Romania and continuously
