ea36ae07dea42a874e223398d5a2d91bc3c3fa5c
Pop Lucas <151132560+lucaspop5511@users.noreply.github.com>
2024-11-06 23:44:34 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function Hero() {
     return (
         <section style={{ textAlign: 'center', marginBottom: '0' }}>
-            <h1>Pop Lucas</h1>
+            <h1>Hi there! I'm Lucas</h1>
             <p>Web Designer and Student, Romania 🇷🇴</p>
         </section>
     );
